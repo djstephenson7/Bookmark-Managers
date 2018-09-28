55ab309bed9e40c9393956cd91b01bf7e185d1e8
djstephenson7 <dan_0502@hotmail.co.uk>
2018-09-28 14:54:10 +0100

    Step 12

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -23,13 +23,14 @@ class Bookmark
     return false unless is_valid?(url)
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'bookmark_manager_test')
+      p connection
     else
       connection = PG.connect(dbname: 'bookmark_manager')
     end
     result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, title, url;")
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
-  
+
   attr_reader :id, :title, :url
 
   def initialize(id:, title:, url:)
